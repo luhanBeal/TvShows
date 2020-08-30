69866cd7e9c8ab930893f545e8a515c490212241
luhan <luhansantanna@gmail.com>
2020-08-30 16:00:13 -0300

    First attempt to create a Django project doo add Tv series Types

---
+++ flixweb/urls.py
@@ -0,0 +1,26 @@
+"""flixweb URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # mandando o que é principal, é para pricurar em configurações de URL em principal
+    path(route='principal/', view=include('principal.urls')),
+    # exempo (principal/tela_de_login, principal/add_item, ...)
+    # ADD PATH DO GENERO
+    path(route='genero/', view=include('genero.urls'))
+]
---
+++ genero/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from genero.models import Genero
+# Register your models here.
+# para aparecer na parte do admin
+admin.site.register(Genero)
